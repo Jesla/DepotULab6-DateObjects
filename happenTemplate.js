9dc23f79ce396b94f630250e3095325668e0c6a3
Jesla <jesslar415@gmail.com>
2016-03-01 11:37:01 -0600

    Month Done

---
+++ happenTemplate.js
@@ -0,0 +1,139 @@
+var library = (function() {
+  return {
+	TimeStamp: (function(){
+   	  return {
+		UnixTimestamp: function(){},
+		UnixMillisecond: function(){}
+	  }
+	})(),
+	Local: (function(){
+	  return {
+		Time: (function() {
+		  return {
+	  	    WithSeconds: function(){},
+	   	    WithOutSeconds: function() {}
+		  }
+		})(),
+		MDY: (function(){
+	  	  return {
+		    Numeral: function(){},
+			Name: function(){}
+		  }
+		  })(),
+		}
+	})(),
+	Second: (function(){
+		return{
+			Second: function(){},
+			DblDigit: function(){}
+		}
+	})(),
+	Minute: (function(){
+		return{
+			Minute: function(){},
+			DblDigit: function(){}
+		}
+	})(),
+	Hour: (function(){
+		return {
+			TwentyFourHour: function() {},
+			TwelveHour: function() {},
+			AMPM: (function() {
+				return {
+					UpperCase: function(){},
+					LowerCase: function(){}
+				}
+			})()
+		}
+	})(),
+	Week: (function(){
+		return {
+			DayOfWeek: function(){},
+			AbrDayOfWeek: function(){},
+			FirstTwoOfWeek: function(){},
+			WeekOfYear: function(){}
+		}
+	})(),
+	Month: (function(){
+		return {
+			DateOfMonth: (function(){
+				return {
+					Numeral: function(){
+                        var date = new Date();
+                        var month = date.getDate();
+                        return String(month);   
+                    },
+					Ordinal: function(){
+                        var date = new Date();
+                        var month = date.getDate();
+                        return String(month + "st");     
+                    },
+					DateDblDigit: function(){
+                        var date = new Date();
+                        var month = date.getDate();
+                        return String("0" + month);
+                    }
+				}
+			})(),
+			// MonthNumber: function(){ return String(new Date().getMonth() + 1);},
+            MonthNumber: function(){
+                var date = new Date();
+                var month = date.getMonth() + 1;
+                return String(month);
+            },
+			MonthNumberDblDigit: function(){
+                var date = new Date();
+                var month = date.getMonth() + 1;
+                return String("0" + month);
+            },
+			AbrOfCurrentMonth: function(){
+                var date = new Date();
+                var month = new Array();
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                return (month[2]).substring(0, 3);
+            },
+			CurrentMonth: function(){
+                var date = new Date();
+                var month = new Array();
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                return (month[2]);
+            }
+		}
+	})(),
+	Year: (function(){
+		return {
+			DayOfYear: (function(){
+				return {
+					Numeral: function(){},
+					Ordinal: function(){}
+				}
+			})(),
+			YearFull: function(){},
+			YearAbr: function(){}
+		}
+	})(),
+	Defaults: function(){}
+  }
+})();
